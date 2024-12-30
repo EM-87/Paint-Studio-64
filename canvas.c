cb91e4f5ea0300ab9fe59ba33e695106f737f14d
Eduardo <Em.87.webs@gmail.com>
2024-12-30 22:04:03 +0100

    Create canvas.c
    #28

---
+++ canvas.c
@@ -0,0 +1,16 @@
+/*
+ * FILE: canvas.c
+ * Minimal or legacy code for handling a basic canvas. 
+ * In advanced versions, we rely on layer_system or canvas_extended for logic.
+ */
+#include "canvas.h"
+#include <stdio.h>
+
+/*
+ * We might have had functions like enable_high_memory_mode(...) 
+ * or enable_default_mode(...) in older versions.
+ * For brevity, we skip reprinting them here if we already moved them
+ * to canvas_extended or layer_system.
+ */
+
+/* ... (This file can remain minimal or be phased out) ... */
